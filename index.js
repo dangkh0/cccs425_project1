16d262d2b8857e6983488e03d8de7b860d62b1e9
Dang Khoi Vo <khoi@air-dang.local>
2020-10-27 00:35:38 -0400

    add get /sourcecode endpoint

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get("/sourcecode", (req, res) => {
+    res.send(require('fs').readFileSync(__filename).toString())
+    })
+
 app.listen(process.env.PORT || 3000)
