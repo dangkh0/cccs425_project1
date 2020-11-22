f6187a0cc87e4c9b9b6edb82f2253116cb2ff0f3
dangkv <vo.dangkh@gmail.com>
2020-11-22 17:25:46 -0500

    Finished /create-channel endpoint

--- README.md
+++ README.md
@@ -27,8 +27,14 @@ Instructions [here](https://wakata.io/webservertestcases/index.html?stream=webse
 | {"success":true,"token":"some-unique-token-23l4"} | `curl -X POST -d '{"username": "stringer_bell", "password": "queen"}' https://onyx-frequent-holiday.glitch.me/login`|
 | {"success":false,"reason":"User does not exist"} | `curl -X POST -d '{"username": "stringer_bel", "password": "queen"}' https://onyx-frequent-holiday.glitch.me/login` |
 | {"success":false,"reason":"Invalid password"} | `curl -X POST -d '{"username": "stringer_bell", "password": "quen"}' https://onyx-frequent-holiday.glitch.me/login` |
-| {"success":false,"reason":"password field missing"} | `curl -X POST -d '{ "username": "queen"}' https://onyx-frequent-holiday.glitch.me/login`|
+| {"success":false,"reason":"password field missing"} | `curl -X POST -d '{ "username": "stringer_bell"}' https://onyx-frequent-holiday.glitch.me/login`|
 | {"success":false,"reason":"username field missing"} | `curl -X POST -d '{ "password": "queen"}' https://onyx-frequent-holiday.glitch.me/login` |
 
 ## /create-channel
-
+| Response | Request |
+| --- | --- |
+| {"success":true} | `curl -XPOST -H 'token: this_is_the_token' -d '{"channelName": "orlandos"}' 'https://onyx-frequent-holiday.glitch.me/create-channel'` |
+| {"success":false,"reason":"token field missing"} | `curl -XPOST -d '{"channelName": "orlandos"}' 'https://onyx-frequent-holiday.glitch.me/create-channel'` |
+| {"success":false,"reason":"Invalid token"} | `curl -XPOST -H 'token: this_is_not_the_token' -d '{"channelName": "orlandos"}' 'https://onyx-frequent-holiday.glitch.me/create-channel'` |
+| {"success":false,"reason":"channelName field missing"} | `curl -XPOST -H 'token: this_is_the_token' -d '{}' 'https://onyx-frequent-holiday.glitch.me/create-channel'`|
+| {"success":false,"reason":"Channel already exists"} | `curl -XPOST -H 'token: this_is_the_token' -d '{"channelName": "thewire"}' 'https://onyx-frequent-holiday.glitch.me/create-channel'` |
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
-const { token } = require('morgan');
 const morgan = require('morgan');
 
 const app = express();
@@ -10,10 +9,19 @@ app.use(bodyParser.raw({ type: "*/*" }));
 app.use(cors());
 app.use(morgan('combined'));
 
-let userDataBase = new Map(); //userId: {username:xyz, password:xyz}
+let channelTable = new Map(); // channelId: {token, channelName}
+let loginTable = new Map(); // token: userId 
+let userTable = new Map(); // userId: {username,password:}
 
-userDataBase.set('xyz', {username: "stringer_bell", password: "queen"});
-userDataBase.set('zyx', {username: "mcnulty", password: "iLoveAlcohol"});
+
+// TEST DATA vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+userTable.set("xyz", {username: "stringer_bell", password: "queen"});
+userTable.set("zyx", {username: "mcnulty", password: "iLoveAlcohol"});
+
+loginTable.set("this_is_the_token", {userId: "zyx"});
+
+channelTable.set("channel-id", {token: "this_is_the_token", channelName: "thewire"});
+// TEST DATA ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 // generate random id
 let genId = () => {
@@ -22,48 +30,81 @@ let genId = () => {
 
 // responses ===================================================================
 
-const SIGNUP_RESPONSES = {
+const GLOBAL_RES = {
+    invalidToken: {"success":false,"reason":"Invalid token"},
+    missingTokenField: {"success":false,"reason":"token field missing"},
+}
+
+const CREATE_CHANNEL_RES = {
+    good: {"success":true},
+    notUniqueChannel: {"success":false,"reason":"Channel already exists"},
+    missingChannelNameField : {"success":false,"reason":"channelName field missing"},
+}
+
+const SIGNUP_RES = {
     good: {"success":true},
     notUnique: {"success":false,"reason":"Username exists"},
     missingNameField: {"success":false,"reason":"username field missing"},
     missingPasswordField: {"success":false,"reason":"password field missing"},
 };
 
-const LOGIN_REPONSES = {
-    good: {"success":true,"token": genId()},
-    userNotExist: {"success":false,"reason":"User does not exist"},
+const LOGIN_RES = {
+    good: {"success":true,"token": undefined},
     invalidPassword: {"success":false,"reason":"Invalid password"},
     missingNameField: {"success":false,"reason":"username field missing"},
     missingPasswordField: {"success":false,"reason":"password field missing"},
+    userNotExist: {"success":false,"reason":"User does not exist"},
 };
 
 // functions and methods =======================================================
 
 // search map by value
-let findValueInMap = (map, value) => {
+let findValueInMap = (map, value, valueKey = undefined) => {
     for (let [k, v] of map) {
-      if (JSON.stringify(v) === JSON.stringify(value)) { 
-        return true; 
-      };
+        // if (valueKey === undefined) {
+            if (JSON.stringify(v) === JSON.stringify(value)) { 
+                return true;
+            } else if (JSON.stringify(v[valueKey]) === JSON.stringify(value)) {
+                return true;
+            };
+        // } else {
+        //     if (JSON.stringify(v[valueKey]) === JSON.stringify(value)) { 
+        //         return true;
+        //     };
+        // };
     };
     return false;
 };
 
-let login = cred => {
-    let validationResponse = loginValidation(cred);
-    console.log("login-attempt: ", cred)
+// function where there is a create channel name request
+let createChannel = (token, newChannel) => {
+    let channelName = newChannel["channelName"]
+    let validationResponse = createChannelName(token, channelName);
     if (validationResponse["success"] === true) {
-        console.log("login-response: ", validationResponse);
+        channelTable.set(genId(), {token: token, channelName:channelName});
+        console.log("channel TABLE: ", channelTable);
+        console.log("createChannel: ", validationResponse);
+        console.log("new channel: ", channelName);
     };
     return validationResponse
 };
 
-// function when there is a signup
-let newSignUp = entry => {
-    let userId = genId();
-    let validationResponse = newSignUpValidation(entry);
+// function when there is a login request
+let login = (cred) => {
+    let validationResponse = loginValidation(cred);
+    console.log("login-attempt: ", cred)
     if (validationResponse["success"] === true) {
-        userDataBase.set(userId, entry);
+        console.log("login: ", validationResponse);
+    };
+    return validationResponse
+};
+
+// function when there is a signup request
+let signUp = (entry) => {
+    let userId = genId();
+    let validationResponse = signUpValidation(entry);
+    if (validationResponse["success"] === true) {
+        userTable.set(userId, entry);
         console.log("newUser: ", userId + ": " + entry);
     };
     return validationResponse;
@@ -72,50 +113,78 @@ let newSignUp = entry => {
 
 // validation functions --------------------------------------------------------
 
+// validation for /create-channel endpoint
+let createChannelName = (token, channelName) => {
+    // validate if token is missing
+    if (token === undefined) {
+        return GLOBAL_RES["missingTokenField"];
+    };
+    // validate if channel name field is missing
+    if (channelName === undefined) {
+        return CREATE_CHANNEL_RES["missingChannelNameField"];
+    };
+    // validate if token is valid
+    if (loginTable.get(token) !== undefined) {
+        // validate if channel name is unique
+        if (findValueInMap(channelTable, channelName, "channelName")) {
+            return CREATE_CHANNEL_RES["notUniqueChannel"]
+        };
+    } else {
+        return GLOBAL_RES["invalidToken"]
+    };
+    return CREATE_CHANNEL_RES["good"]
+};
+
+// validation for /login endpoint
 let loginValidation = cred => {
-    let userIds = Array.from(userDataBase.keys());
+    let userIds = Array.from(userTable.keys());
 
     // valiadate if username field is missing
     if (cred['username'] === undefined) {
-        return LOGIN_REPONSES['missingNameField'];
+        return LOGIN_RES['missingNameField'];
     }
     // valiadate if password field is missing
     if (cred['password'] === undefined) {
-        return LOGIN_REPONSES['missingPasswordField'];
+        return LOGIN_RES['missingPasswordField'];
     }
     // valildate if username exist in userDataBase
     for (let i = 0; i < userIds.length; i++) {
         let id = userIds[i];
-        let password = userDataBase.get(id)["password"];
-        let username = userDataBase.get(id)["username"];
+        let password = userTable.get(id)["password"];
+        let username = userTable.get(id)["username"];
         
         if (cred['username'] === username) {
             if (cred['password'] === password) {
                 let token  = genId();
-                LOGIN_REPONSES['good']['token'] = token;
-                return LOGIN_REPONSES['good'];
+                loginTable.set(token, {userId: id});
+                LOGIN_RES['good']['token'] = token;
+                console.log("Login Response: ", LOGIN_RES["good"]);
+                return LOGIN_RES['good'];
             } else {
-                return LOGIN_REPONSES['invalidPassword'];
+                return LOGIN_RES['invalidPassword'];
             };
         };
     };
-    return LOGIN_REPONSES['userNotExist'];
+    return LOGIN_RES['userNotExist'];
 };
 
-let newSignUpValidation = entry => {
+// validation for /signup endpoint
+let signUpValidation = entry => {
     // valiadate if username field is missing
-    if (entry['username'] === undefined) {
-        return SIGNUP_RESPONSES['missingNameField'];
+    if (entry["username"] === undefined) {
+        return SIGNUP_RES['missingNameField'];
     }
     // valiadate if password field is missing
-    if (entry['password'] === undefined) {
-        return SIGNUP_RESPONSES['missingPasswordField'];
+    if (entry["password"] === undefined) {
+        return SIGNUP_RES['missingPasswordField'];
     }
     // validate unique entry
-    if (!findValueInMap(userDataBase, entry)) {
-        return SIGNUP_RESPONSES['good'];
+    if (!findValueInMap(userTable, entry["username"], "username")) {
+        console.log("userTable: ", userTable);
+        console.log("entry: ", entry); 
+        return SIGNUP_RES['good'];
     } else {
-        return SIGNUP_RESPONSES['notUnique'];
+        return SIGNUP_RES['notUnique'];
     };
 }
 
@@ -126,33 +195,43 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
+app.get("/sourcecode", (req, res) => {
+    res.send(require('fs').readFileSync(__filename).toString())
+  });
+
 // POST ------------------------------------------------------------------------
-app.post("/signup", (req, res) => {
-    let entry = undefined
-    let parsed = JSON.parse(req.body);
+app.post("/create-channel", (req, res) => {
+    let body = JSON.parse(req.body);
+    let token = req.headers["token"];
+    let newChannel = undefined;
 
-    console.log('body: ', req.body);
-    console.log('body: ', parsed);
+    console.log("/create-channel", body);
 
-    entry = {username: parsed.username.toLowerCase(), password: parsed.password};
-
-    res.send(newSignUp(entry));
+    newChannel = {channelName: body.channelName};
+    debugger
+    res.send(createChannel(token, newChannel));
 });
 
 app.post("/login", (req, res) => {
     let cred  = undefined;
-    let parsed = JSON.parse(req.body);
-    debugger
-    console.log("body: ", req.body);
-    console.log("body: ", parsed);
+    let body = JSON.parse(req.body);
 
-    cred = {username: parsed.username.toLowerCase(), password: parsed.password};
+    console.log("/login: ", body);
+
+    cred = {username: body.username, password: body.password};
     
     res.send(login(cred));
 });
 
-app.get("/sourcecode", (req, res) => {
-  res.send(require('fs').readFileSync(__filename).toString())
+app.post("/signup", (req, res) => {
+    let entry = undefined
+    let body = JSON.parse(req.body);
+
+    console.log('/signup: ', body);
+
+    entry = {username: body.username, password: body.password};
+
+    res.send(signUp(entry));
 });
 
 // other -----------------------------------------------------------------------
